1bbe13db7a3e675742e7c184bae45bd8018e603b
83315 <alshaibaninada@gmail.com>
2024-04-15 11:44:36 +0300

    Add project files.

---
+++ ConsoleApp5/Account.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public int AccountNumber { get; set; }
+        public double Balance { get; set; }
+        private Transaction[] transactionHistory;
+        private int transactionIndex;
+
+        public Account(string name, int accountNumber, double balance)
+        {
+            Name = name;
+            AccountNumber = accountNumber;
+            Balance = balance;
+            this.transactionHistory = new Transaction[100];
+            this.transactionIndex = 0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+            Transaction transaction = new Transaction(DateTime.Now, (decimal)amount, "Deposit");
+            transactionHistory[transactionIndex] = transaction;
+            transactionIndex++;
+            Console.WriteLine($"Deposited {amount} KWD to the account. New balance: {Balance} KWD");
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                Transaction transaction = new Transaction(DateTime.Now, (decimal)amount, "Withdrawal");
+                transactionHistory[transactionIndex] = transaction;
+                transactionIndex++;
+                Console.WriteLine($"Withdrew {amount} KWD from the account. New balance: {Balance} KWD");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance.");
+            }
+        }
+
+        public void getBalance()
+        {
+            Console.WriteLine($"Account Balance: {Balance} KWD");
+        }
+
+        public void PrintTransactionHistory()
+        {
+            Console.WriteLine("Transaction history:");
+            for (int i = 0; i < transactionIndex; i++)
+            {
+                Transaction transaction = transactionHistory[i];
+                Console.WriteLine($"{transaction.Date.ToShortDateString()} - {transaction.Type} - Amount: {transaction.Amount} KWD");
+            }
+        }
+    }
+
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Type { get; set; }
+
+        public Transaction(DateTime date, decimal amount, string type)
+        {
+            Date = date;
+            Amount = amount;
+            Type = type;
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp5;
+ static Account InitializeAccount()
+{
+
+    Console.WriteLine("Welcome to the Basic Banking Application ");
+
+    Console.WriteLine("Enter your name: ");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Enter your initial account number: ");
+    int accountNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter your initial balance amount: ");
+    double balance = Convert.ToDouble(Console.ReadLine());
+
+    return new Account(name, accountNumber, balance);
+
+
+    Console.WriteLine("Account setup successful! ");
+}
+Account account = InitializeAccount(); 
+
+bool exit = false;
+
+while (!exit)
+{
+    Console.WriteLine("\nSelect an option:");
+    Console.WriteLine("1: Deposit");
+    Console.WriteLine("2: Withdraw");
+    Console.WriteLine("3: View Balance");
+    Console.WriteLine("4: View Transaction History");
+    Console.WriteLine("5: Exit");
+
+    int option = Convert.ToInt32(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+            Console.Write("Enter deposit amount: ");
+            double depositAmount = Convert.ToDouble(Console.ReadLine());
+            account.Deposit(depositAmount);
+            break;
+        case 2:
+            Console.Write("Enter withdrawal amount: ");
+            double withdrawalAmount = Convert.ToDouble(Console.ReadLine());
+            account.Withdraw(withdrawalAmount);
+            break;
+        case 3:
+            account.getBalance();
+            break;
+        case 4:
+            account.PrintTransactionHistory();
+            break;
+        case 5:
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Invalid option, enter another option");
+            break;
+    }
+//    Console.WriteLine("\nDo you want to perform another transaction? (y/n)");
+//    string response = Console.ReadLine();
+//    if (response.ToLower() != "y")
+//    {
+//        exit = true;
+//    }
+
+//account.PrintTransactionHistory();
+}
